5a39fd455e70d4ca162388127c12cd080d42e311
Max Williams <max@pusher.com>
2016-04-28 14:38:30 +0100

    Added more sprites

--- game.js
+++ game.js
@@ -6,7 +6,8 @@ var playerSprite = {
 }
 var sprites = {
   players: [],
-  loot: []
+  loot: [],
+  dirt: []
 }
 
 var player = {
@@ -20,6 +21,7 @@ var ctx
 var playerSpriteImg
 var dispatcher = []
 var fullHealth = 1000
+var moves = {x: 0, y: 0}
 var viewPort = {
   width: 700,
   height: 700,
@@ -44,13 +46,28 @@ $().ready(function(){
   playerSpriteImg.onload = function(){
     imagesReady = true
   };
-  playerSpriteImg.src = 'images/player-sprite.png';
+  playerSpriteImg.src = 'images/raza54bbig1_1.png';
+  
   medPakImg = new Image();
   medPakImg.onload = function(){
     imagesReady = true
   };
   medPakImg.src = 'images/medpak.png';
-  $(document).keydown( handleKeys )
+  
+  grassImg = new Image();
+  grassImg.onload = function(){
+    imagesReady = true
+  };
+  grassImg.src = 'images/grass_tile.jpg';
+  
+  dirtImg = new Image();
+  dirtImg.onload = function(){
+    imagesReady = true
+  };
+  dirtImg.src = 'images/grass_dirt_tile.jpg';
+  
+  $(document).keydown( handleKeyDown )
+  $(document).keyup( handleKeyUp )
   
   startGame()
 })
@@ -58,6 +75,7 @@ $().ready(function(){
 function startGame(){
   addWelcomeMsg()
   addLootSprites()
+  addDirtSprites()
   player.timeRemaining = 1000
   gameInterval = setInterval(gameTick, 50)
   tick = 0
@@ -86,7 +104,7 @@ function addWelcomeMsg(){
 }
 
 var moveKeys = {}
-function handleKeys(event){
+function handleKeyDown(event){
   if ( event.which == 37 ) {
     moveKeys.left = true
     event.preventDefault();
@@ -105,9 +123,28 @@ function handleKeys(event){
   }
 }
 
+function handleKeyUp(event){
+  if ( event.which == 37 ) {
+    moveKeys.left = false
+    event.preventDefault();
+  } 
+  if ( event.which == 39 ) {
+    moveKeys.right = false
+    event.preventDefault();
+  } 
+  if ( event.which == 38 ) {
+    moveKeys.up = false
+    event.preventDefault();
+  } 
+  if ( event.which == 40 ) {
+    moveKeys.down = false
+    event.preventDefault();
+  }
+}
+
 function addLootSprites(){
   var regionSize = 10000
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 50; i++) {
     sprites.loot.push({
       coords: {
         x: Math.round(Math.random() * regionSize) - regionSize/2,
@@ -117,23 +154,19 @@ function addLootSprites(){
   }
 }
 
-var speed = 20
-function movePlayerSprite(dir){
-  switch(dir){
-  case "left":
-    playerSprite.coords.x -= speed;
-    break;
-  case "right":
-    playerSprite.coords.x += speed;
-    break;
-  case "up":
-    playerSprite.coords.y -= speed
-    break;
-  case "down":
-    playerSprite.coords.y += speed
-    break;
+function addDirtSprites(){
+  var regionSize = 10000
+  for (var i = 0; i < 1000; i++) {
+    sprites.dirt.push({
+      coords: {
+        x: Math.round(Math.random() * regionSize) - regionSize/2,
+        y: Math.round(Math.random() * regionSize) - regionSize/2
+      }
+    })
   }
 }
+
+var speed = 20
 
 var tick
 var welcomeDuration = 100
@@ -148,28 +181,73 @@ function gameTick(){
   render()
   tick += 1
 }
+
 function render(){
   var remainingPercent = Math.round( ( player.timeRemaining / fullHealth ) * 100 )
   $('#timeRemainingIndicator').text( remainingPercent )
   $('#timeRemainingIndicator').css("width",  remainingPercent + "%")
   $('#timeElapsedIndicator').html(player.timeElapsed)
-  if (imagesReady){
-    var c = calculateLocalCoords(playerSprite.coords, viewPort)
-    ctx.drawImage(playerSpriteImg, c.x, c.y)
-  }
+  drawGrass()
+  sprites.dirt.forEach(function(dirtSprite){
+    var c = calculateLocalCoords(dirtSprite.coords, viewPort)
+    ctx.drawImage(dirtImg, c.x, c.y)
+  })
   sprites.loot.forEach(function(lootSprite){
     var c = calculateLocalCoords(lootSprite.coords, viewPort)
     ctx.drawImage(medPakImg, c.x, c.y)
   })
-  
+  drawPlayerSprite()
   drawMiniMap()
+}
+var dir = "down"
+function drawPlayerSprite(){
+  var clipX = 0
+  var clipY = 250
+  if (moves.y < 0 && moves.x == 0 ){
+    var clipX = 45
+    var clipY = 0
+  } 
+  if (moves.y > 0 && moves.x == 0 ){
+    var clipX = 0
+    var clipY = 0
+  }
+  if (moves.x < 0 && moves.y == 0 ){
+    var clipX = 140
+    var clipY = 0
+  } 
+  if (moves.x > 0 && moves.y == 0 ){
+    var clipX = 90
+    var clipY = 0
+  }     
+  // if (moves.y < 0)
+    var clipY = 0
+  var c = calculateLocalCoords(playerSprite.coords, viewPort)
+  ctx.drawImage(playerSpriteImg, clipX, clipY, 50, 55, c.x, c.y, 50, 60)
+}
+
+function drawGrass(){
+  var widthTiles = (viewPort.width / 64) + 1
+  var heightTiles = (viewPort.height / 64) + 1
+  for (var x = 0; x < widthTiles; x++) {
+    for (var y = 0; y < heightTiles; y++) {
+      ctx.drawImage(grassImg, x * 64 - (64 - playerSprite.coords.x % 64), y * 64 - (64 - playerSprite.coords.y % 64))
+    }
+  }
 }
 
 function calculatePlayerMoves(){
-  for (var k in moveKeys){
-    movePlayerSprite(k)
-    delete moveKeys[k]
-  }
+  moves = {x: 0, y: 0}
+  if (moveKeys.left)
+    moves.x -= speed;
+  if (moveKeys.right)
+    moves.x += speed;
+  if (moveKeys.up)
+    moves.y -= speed
+  if (moveKeys.down)
+    moves.y += speed
+    
+  playerSprite.coords.x += moves.x
+  playerSprite.coords.y += moves.y
 }
 
 var frameTime = 1
@@ -188,8 +266,8 @@ function addLoot(){
 function calculateCollisions(){
   var i = 0;
   sprites.loot.forEach(function(lootSprite){
-    if (Math.abs(playerSprite.coords.x - lootSprite.coords.x) < 50 &&
-      Math.abs(playerSprite.coords.y - lootSprite.coords.y) < 50){
+    if (Math.abs(playerSprite.coords.x - lootSprite.coords.x) < 40 &&
+      Math.abs(playerSprite.coords.y - lootSprite.coords.y) < 40){
       var looted = sprites.loot.splice(i, 1)
       addLoot(looted)
     }
